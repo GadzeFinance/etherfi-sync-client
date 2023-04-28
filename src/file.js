a4b98ce3d4d7bf7ea3607626f1aafb86edb78025
Rupert <rupert@linumlabs.com>
2023-04-28 18:07:04 +0200

    add fix for directory for multiple NOs

--- src/file.js
+++ src/file.js
@@ -1,6 +1,10 @@
 import fs from 'fs'
+import { getConfig } from './config.js'
 
 export const createFSBidOutput = (location, data, identifier, validatorPubKey) => {
+
+    const { ETHERFI_SC_CONSENSUS_LOCATION, ETHERFI_SC_CLIENT_LOCATION } = getConfig()
+
     const locationWithBidIdentifier = `${location}/${identifier}`
     createDirSafe(location)
     createDirSafe(locationWithBidIdentifier)
@@ -11,8 +15,8 @@ export const createFSBidOutput = (location, data, identifier, validatorPubKey) =
     // some assumptions are being made on the directories being used below, could be extracted to ENV variables
     const bashHeader = '#!/bin/bash -xe \n'
     const echoLine = `echo "Adding keystore to prysm for validator with pubkey:${validatorPubKey.slice(0, 10)} ..." \n`
-    const changeDirectoryLine = 'cd /home/ec2-user/ethereum/consensus \n'
-    const prysmCommandLine = `sudo ./prysm.sh validator accounts import --goerli --wallet-dir=/home/ec2-user/ethereum/consensus --keys-dir=/home/ec2-user/ethereum/consensus/etherfi-sync-client/storage/output`
+    const changeDirectoryLine = `cd ${ETHERFI_SC_CONSENSUS_LOCATION} \n`
+    const prysmCommandLine = `sudo ./prysm.sh validator accounts import --goerli --wallet-dir=${ETHERFI_SC_CONSENSUS_LOCATION} --keys-dir=${ETHERFI_SC_CLIENT_LOCATION}/storage/output`
 
     createFile(`${locationWithBidIdentifier}/add.sh`, `${bashHeader} ${echoLine} ${changeDirectoryLine} ${prysmCommandLine}/${identifier}/${data.keystoreName}`)
 }
